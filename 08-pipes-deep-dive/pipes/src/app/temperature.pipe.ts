98ed5dd6d378bcdeb36ff8e0e9abead10fe81d65
devemre <dev.emre.aydin@gmail.com>
2024-11-08 22:08:00 +0300

    Section 8: Transforming Values with Pipes - Deep Dive

---
+++ 08-pipes-deep-dive/pipes/src/app/temperature.pipe.ts
@@ -0,0 +1,42 @@
+import { Pipe } from '@angular/core';
+
+@Pipe({
+  name: 'temperature',
+  standalone: true,
+})
+export class TemperaturePipe {
+  transform(
+    value: string | number | null,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    if (!value) {
+      return null;
+    }
+
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
+}
