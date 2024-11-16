1aaa2fd76368cc6dcd651216f58fd5d7e9df707f
devemre <dev.emre.aydin@gmail.com>
2024-11-16 16:29:32 +0300

    Section 14: Routing & Building Multi-page Single Page Applications

---
+++ 14-routing/routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,55 @@
+import { Component, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  userName = input.required<string>();
+  title = input.required<string>();
+
+  /*
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+    });
+  }
+  */
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+
+  const userName =
+    usersService.users.find(
+      (user) => user.id === activatedRoute.params['userId']
+    )?.name || '';
+
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  return resolveUserName(activatedRoute, routerState) + "'s Tasks";
+};
